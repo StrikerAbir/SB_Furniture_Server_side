63b6587c7d1977115dca1146d4cb073137022747
Abir Hasan <cse.170201063@gmail.com>
2022-11-25 07:27:58 +0600

    jwt setup done

--- index.js
+++ index.js
@@ -9,10 +9,89 @@ const jwt = require("jsonwebtoken");
 app.use(cors());
 app.use(express.json());
 
+// variable
+const user = process.env.DB_USER;
+const password = process.env.DB_PASS;
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${user}:${password}@cluster0.nvx6pod.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  console.log(authHeader);
+  if (!authHeader) {
+    return res.status(401).send("unauthorized access");
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, secret, function (err, decoded) {
+    if (err) {
+      // console.log(err);
+      return res.status(403).send({ message: "forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
+async function run() {
+    try {
+      const categoriesCollection = client
+        .db("sbFurniture")
+        .collection("categories");
+      const productsCollection = client
+        .db("sbFurniture")
+        .collection("products");
+      const usersCollection = client
+        .db("sbFurniture")
+        .collection("users");
+
+      //* JWT
+      app.get("/jwt", async (req, res) => {
+        const email = req.query.email;
+        const query = { email: email };
+        const user = await usersCollection.findOne(query);
+        console.log(user);
+        if (user) {
+          const token = jwt.sign({ email }, secret, { expiresIn: "1h" });
+          return res.send({ accessToken: token });
+        }
+        return res.status(403).send({ accessToken: "" });
+      });
+        
+       
+        
+      //* categories
+      app.get("/categories", async (req, res) => {
+        const query = {};
+        const categories = await categoriesCollection.find(query).toArray();
+        // console.log(categories);
+        res.send(categories);
+      });
+
+      //* single category products
+      app.get("/categories/products", async (req, res) => {
+        const id = parseInt(req.query.cat_id);
+        const filter = { category_id: id };
+        const products = await productsCollection.find(filter).toArray();
+        res.send(products);
+      });
+    }
+    finally {
+        
+    }
+}
+run().catch(console.log);
+
+
 app.get("/", async (req, res) => {
-  res.send("SB running");
+  res.send("SB Furniture is running");
 });
 
 app.listen(port, () => {
-  console.log("SB running on", port);
+  console.log("SB furniture running on", port);
 });
