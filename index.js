6ca0928e3b31561431c0ef15fd990f462f500fd5
Abir Hasan <cse.170201063@gmail.com>
2022-11-25 08:08:49 +0600

    user post api done

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(express.json());
 // variable
 const user = process.env.DB_USER;
 const password = process.env.DB_PASS;
+const secret = process.env.ACCESS_TOKEN;
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${user}:${password}@cluster0.nvx6pod.mongodb.net/?retryWrites=true&w=majority`;
@@ -39,54 +40,59 @@ function verifyJWT(req, res, next) {
 }
 
 async function run() {
-    try {
-      const categoriesCollection = client
-        .db("sbFurniture")
-        .collection("categories");
-      const productsCollection = client
-        .db("sbFurniture")
-        .collection("products");
-      const usersCollection = client
-        .db("sbFurniture")
-        .collection("users");
+  try {
+    const categoriesCollection = client
+      .db("sbFurniture")
+      .collection("categories");
+    const productsCollection = client.db("sbFurniture").collection("products");
+    const usersCollection = client.db("sbFurniture").collection("users");
 
-      //* JWT
-      app.get("/jwt", async (req, res) => {
-        const email = req.query.email;
-        const query = { email: email };
-        const user = await usersCollection.findOne(query);
-        console.log(user);
-        if (user) {
-          const token = jwt.sign({ email }, secret, { expiresIn: "1h" });
-          return res.send({ accessToken: token });
-        }
-        return res.status(403).send({ accessToken: "" });
-      });
-        
-       
-        
-      //* categories
-      app.get("/categories", async (req, res) => {
-        const query = {};
-        const categories = await categoriesCollection.find(query).toArray();
-        // console.log(categories);
-        res.send(categories);
-      });
+    //* JWT
+    app.get("/jwt", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      console.log(user);
+      if (user) {
+        const token = jwt.sign({ email }, secret, { expiresIn: "7d" });
+        return res.send({ accessToken: token });
+      }
+      return res.status(403).send({ accessToken: "" });
+    });
 
-      //* single category products
-      app.get("/categories/products", async (req, res) => {
-        const id = parseInt(req.query.cat_id);
-        const filter = { category_id: id };
-        const products = await productsCollection.find(filter).toArray();
-        res.send(products);
-      });
-    }
-    finally {
-        
-    }
+    //* Users
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const filter = { email: user.email };
+      const email = await usersCollection.findOne(filter);
+      if (email) {
+        res.send({ message: "Already have account wth this email." });
+      } else {
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+      }
+    });
+
+    //* categories
+    app.get("/categories", async (req, res) => {
+      const query = {};
+      const categories = await categoriesCollection.find(query).toArray();
+      // console.log(categories);
+      res.send(categories);
+    });
+
+    //* single category products
+    app.get("/categories/products", async (req, res) => {
+      const id = parseInt(req.query.cat_id);
+      const filter = { category_id: id };
+      const products = await productsCollection.find(filter).toArray();
+      res.send(products);
+    });
+  } finally {
+  }
 }
 run().catch(console.log);
-
 
 app.get("/", async (req, res) => {
   res.send("SB Furniture is running");
